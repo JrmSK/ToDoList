6665907a8198d9396f09ffc269c156419c49211d
JrmSK <jeremy.skornik@gmail.com>
2018-12-08 20:56:01 +0200

    Added an alarm to activities, with a modal to select time before alarm

--- logic.js
+++ logic.js
@@ -6,7 +6,9 @@ class App extends React.Component {
             placeholder: "Enter something ToDo",
             doneActivities: [],
             toDoActivities: [],
-            counter: 0
+            counter: 0,
+            isOpen: false,
+            activityContent: "",
         };
         this.addItem = this.addItem.bind(this);
         this.moveToDoneList = this.moveToDoneList.bind(this);
@@ -15,6 +17,8 @@ class App extends React.Component {
         this.setFavorite = this.setFavorite.bind(this);
         this.changeDateFormat = this.changeDateFormat.bind(this);
         this.playSound = this.playSound.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+
 
     }
 
@@ -74,8 +78,8 @@ class App extends React.Component {
 
     }
 
-     /* Give or remove favorite classname to selected ToDo item */
-     setFavorite(e) {
+    /* Give or remove favorite classname to selected ToDo item */
+    setFavorite(e) {
         var text = e.target.parentElement;
         if (e.target.parentElement.classList.contains("favorite")) {
             document.getElementById("toDo-Container").append(text);
@@ -90,6 +94,13 @@ class App extends React.Component {
     playSound(type) {
         document.getElementById("sound").src = `./sound/${type}.mp3`;
         document.getElementById("sound").play();
+    }
+
+    toggleModal(e) {
+        this.setState({
+            isOpen: !this.state.isOpen,
+            activityContent: e.target.parentElement.textContent
+        });
     }
 
     render() {
@@ -108,12 +119,13 @@ class App extends React.Component {
                 </div>
                 <h2 className="title">To Do</h2>
                 <div className="toDo-Container">
-                    <ToDoList activities={this.state.activities} toDoActivities={this.state.toDoActivities} handleDelete={this.delete} handleFavorite={this.setFavorite} handleClick={this.moveToDoneList} />
+                    <ToDoList activities={this.state.activities} toDoActivities={this.state.toDoActivities} handleDelete={this.delete} handleFavorite={this.setFavorite} handleModal={this.toggleModal} handleClick={this.moveToDoneList} />
                 </div>
                 <h2 className="title">Done</h2>
                 <div className="done-Container">
                     <DoneList doneActivities={this.state.doneActivities} handleClick={this.moveToToDoList} handleDelete={this.delete} />
                 </div>
+                <Modal show={this.state.isOpen} onClose={this.toggleModal} contentHandle={this.state.activityContent} handleSound={this.playSound} />
             </div>
         );
     }
@@ -125,6 +137,7 @@ class ToDoList extends React.Component {
         this.select = this.select.bind(this);
         this.selectDelete = this.selectDelete.bind(this);
         this.selectFavorite = this.selectFavorite.bind(this);
+        this.openModal = this.openModal.bind(this);
         this.state = {
             toDoActivities: this.props.toDoActivities,
         }
@@ -148,14 +161,21 @@ class ToDoList extends React.Component {
         this.props.handleFavorite(e);
     }
 
+    openModal(e) {
+        this.props.handleModal(e);
+    }
+
     render() {
         return (
             <ul id="toDo-Container">
                 {/* Will generate Todo's from user input */}
                 {this.props.activities.map((activity, i) => <li key={i}>
                     <input className="inputImage" onClick={this.select} type="image" src="./images/tick.png" />
-                    <input className="inputImage" onClick={this.selectFavorite}   type="image" src="./images/star.png" />
-                    <input className="inputImage" onClick={this.selectDelete} type="image" src="./images/bin.png" />{this.generateActivityString(activity)}</li>
+                    <input className="inputImage" onClick={this.selectFavorite} type="image" src="./images/star.png" />
+                    <input className="inputImage" onClick={this.selectDelete} type="image" src="./images/bin.png" />
+                    <input className="inputImage clock" onClick={this.openModal} type="image" src="./images/clock.png" />
+                    {this.generateActivityString(activity)}</li>
+
                 )}
                 {/* Will generate Todo's from reset of done list item  */}
                 {this.props.toDoActivities.map((i) => <li key={i.index}>
@@ -197,6 +217,65 @@ class DoneList extends React.Component {
         );
     }
 }
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 5,
+        }
+        this.alarmSet = this.alarmSet.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.playSound = this.playSound.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    /* Set the alarm for the selected activity, with the selected amount of time */
+    alarmSet() {
+        var that = this;
+        setTimeout(function () {
+            var activity = that.props.contentHandle;
+            that.playSound("star");
+            var aud = document.getElementById("sound");
+            aud.onended = function () {                     // this was necessary because the sound was played after the alert otherwise
+                alert(`dont forget: ${activity}`);
+            }
+        }, (this.state.value * 60000))
+    }
+
+    playSound(arg) {
+        this.props.handleSound(arg);
+    }
+
+    render() {
+        // Render nothing if the "show" prop is false
+        if (!this.props.show) {
+            return null;
+        }
+
+        return (
+            <div className="backdrop" className="modal-backdrop">
+                <div className="modal">
+                    <p>When shall I remind you of your task?</p>
+                    <select className="modal-element" value={this.state.value} onChange={this.handleChange} selected={5} id="time-selected">
+                        <option value={5}>5mn</option>
+                        <option value={10}>10mn</option>
+                        <option value={60}>1h</option>
+                        <option value={120}>2h</option>
+                        <option value={1440}>1 day</option>
+                    </select>
+                    <input className="modal-element" type="button" onMouseDown={this.alarmSet} onClick={this.props.onClose} value="remind me" />
+                </div>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
     <App />,
